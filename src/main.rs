4c50f1740ef3ed74593de5825433b8a420ae3e8c
Ryan Chandler <ryangjchandler@gmail.com>
2021-10-20 20:20:46 +0100

    chore: tidy up cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env::args;
+use std::net::TcpListener;
 
 mod cmd;
 mod token;
@@ -14,28 +15,37 @@ fn main() {
         cmd::help();
     }
 
-    let file = match args().nth(1) {
-        Some(f) => f,
-        None => {
-            eprintln!("Please provide a file path.");
-            std::process::exit(1);
-        }
+    let command = match args().nth(1) {
+        Some(c) => c,
+        _ => unreachable!()
     };
 
-    let contents = match std::fs::read_to_string(file) {
-        Ok(s) => s,
-        Err(_) => {
-            eprintln!("Unable to open file.");
-            std::process::exit(1);
-        }
-    };
+    match command.as_str() {
+        _ => {
+            let file = match args().nth(1) {
+                Some(f) => f,
+                None => {
+                    eprintln!("Please provide a file path.");
+                    std::process::exit(1);
+                }
+            };
 
-    let tokens = token::generate(&contents);
-    let ast = parser::parse(tokens);
-    let (constants, code, globals) = compiler::compile(ast);
+            let contents = match std::fs::read_to_string(file) {
+                Ok(s) => s,
+                Err(_) => {
+                    eprintln!("Unable to open file.");
+                    std::process::exit(1);
+                }
+            };
 
-    #[cfg(debug_assertions)]
-    dbg!(&constants, &code, &globals);
-    
-    vm::run(constants, code, globals);
+            let tokens = token::generate(&contents);
+            let ast = parser::parse(tokens);
+            let (constants, code, globals) = compiler::compile(ast);
+
+            #[cfg(debug_assertions)]
+            dbg!(&constants, &code, &globals);
+            
+            vm::run(constants, code, globals);
+        },
+    }
 }
